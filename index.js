ece2462d1470b45a954ed995be6f24dd357f28d5
DOANANHQUAN <97490594+DOANANHQUAN@users.noreply.github.com>
2022-07-18 15:25:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function stop() {
     var score = document.getElementById('number').value;
     alert('Chúc mừng!!! Điểm của bạn là: ' + score);
     if (score < 200) {
-        alert('200 điểm trở lên mới đậu,dưới trung bình, loại:)))))');
+        alert('200 điểm trở lên mới đậu, dưới trung bình, loại:)))))');
         document.body.style.display = 'none';
         document.write('<h1 style="font-family: sans-serif;">Oh no loại rồi, ấn reset để làm lại cuộc đời:)))</h1><div style="font-family: sans-serif;font-size: 25px;"><center><a style="text-decoration: none;" <a href="http://doananhquan.github.io/vongquaymayman/">Reset</a></center></div>')
     };
