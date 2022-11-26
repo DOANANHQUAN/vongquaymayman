29750518f29506756e76edbb552aac542769e571
DOANANHQUAN <97490594+DOANANHQUAN@users.noreply.github.com>
2022-11-26 16:12:16 +0700

    UBPDTARTE

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 var clear;
+var Colors = [
+    "rgb(252, 78, 78)",
+    "rgb(85, 245, 49)",
+    "rgb(49, 167, 245)",
+    "white",
+    "#e28743",
+    "blue"
+]
+var MyEYE = Colors[Math.floor(Math.random() * Colors.length)];
 var getValue = function () {
     var number = document.getElementById('number').value;
     number++;
@@ -9,6 +18,7 @@ var getValue = function () {
 }
 function start() {
    clear =  setInterval(getValue, 1);
+   document.body.style.backgroundColor = MyEYE;
 }
 function stop() {
     clearInterval(clear);
@@ -20,4 +30,9 @@ function stop() {
         document.write('<h1 style="font-family: sans-serif;">Oh no loại rồi, ấn reset để làm lại cuộc đời:)))</h1><div style="font-family: sans-serif;font-size: 25px;"><center><a style="text-decoration: none" href="http://doananhquan.github.io/vongquaymayman/">Reset</a></center></div>')
     };
     document.getElementById('number').value = 0;
+    window.location.href = 'https://doananhquan.github.io/vongquaymayman/';
 }
+// while (true){
+//     var MyEYE = Colors[Math.floor(Math.random() * Colors.length)];
+//     document.getElementById("number").style.color = MyEYE;
+// }
